13663fe07823dbe3b187dc65ba3ba36fea7d8931
Stylesmiel <3239866994@qq.com>
2024-02-16 14:38:00 +0800

    数据库默认格式设置为utf8mb4

--- im-platform/src/main/resources/db/db.sql
+++ im-platform/src/main/resources/db/db.sql
@@ -1,4 +1,4 @@
-use `box-im`;
+use `fastboot-im`;
 create table `im_user`(
     `id` bigint not null auto_increment primary key  comment 'id',
     `user_name` varchar(255) not null comment '用户名',
@@ -13,7 +13,7 @@ create table `im_user`(
     `created_time` datetime DEFAULT CURRENT_TIMESTAMP comment '创建时间',
     unique key `idx_user_name`(user_name),
     key `idx_nick_name`(nick_name)
-) ENGINE=InnoDB CHARSET=utf8mb3 comment '用户';
+) ENGINE=InnoDB CHARSET=utf8mb4 comment '用户';
 
 create table `im_friend`(
     `id` bigint not null auto_increment primary key  comment 'id',
@@ -24,7 +24,7 @@ create table `im_friend`(
     `created_time` datetime DEFAULT CURRENT_TIMESTAMP comment '创建时间',
     key `idx_user_id` (`user_id`),
     key `idx_friend_id` (`friend_id`)
-) ENGINE=InnoDB CHARSET=utf8mb3 comment '好友';
+) ENGINE=InnoDB CHARSET=utf8mb4 comment '好友';
 
 create table `im_private_message`(
     `id` bigint not null auto_increment primary key comment 'id',
@@ -35,7 +35,7 @@ create table `im_private_message`(
     `status` tinyint(1) NOT NULL   comment '状态 0:未读 1:已读 2:撤回',
     `send_time` datetime DEFAULT CURRENT_TIMESTAMP comment '发送时间',
     key `idx_send_recv_id` (`send_id`,`recv_id`)
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '私聊消息';
+)ENGINE=InnoDB CHARSET=utf8mb4 comment '私聊消息';
 
 
 create table `im_group`(
@@ -48,7 +48,7 @@ create table `im_group`(
     `remark` varchar(255) default '' comment '群备注',
     `deleted` tinyint(1) default 0   comment '是否已删除',
     `created_time` datetime default CURRENT_TIMESTAMP comment '创建时间'
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '群';
+)ENGINE=InnoDB CHARSET=utf8mb4 comment '群';
 
 create table `im_group_member`(
     `id` bigint not null auto_increment primary key comment 'id',
@@ -61,7 +61,7 @@ create table `im_group_member`(
     `created_time` datetime DEFAULT CURRENT_TIMESTAMP comment '创建时间',
     key `idx_group_id`(`group_id`),
     key `idx_user_id`(`user_id`)
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '群成员';
+)ENGINE=InnoDB CHARSET=utf8mb4 comment '群成员';
 
 create table `im_group_message`(
     `id` bigint not null auto_increment primary key comment 'id',
@@ -74,4 +74,4 @@ create table `im_group_message`(
     `status` tinyint(1) DEFAULT 0 comment '状态 0:正常  2:撤回',
     `send_time` datetime DEFAULT CURRENT_TIMESTAMP comment '发送时间',
     key `idx_group_id` (group_id)
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '群消息';
+)ENGINE=InnoDB CHARSET=utf8mb4 comment '群消息';
