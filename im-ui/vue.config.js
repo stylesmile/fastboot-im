d2fcce6d1f8e9e4930be890a451ae4b6fb7eaf81
Stylesmiel <3239866994@qq.com>
2024-02-16 14:20:36 +0800

    im-ui启动方式优化

--- im-ui/vue.config.js
+++ im-ui/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8888',
+        target: 'http://127.0.0.1:7070',
         changeOrigin: true,
         ws: false,
         pathRewrite: {
@@ -12,5 +12,5 @@ module.exports = {
       }
     }
   }
- 
+
 }
