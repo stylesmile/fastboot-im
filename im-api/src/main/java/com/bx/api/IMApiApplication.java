69ae602079ab6253a9d09f8654ad1409c03cead2
Stylesmile <3239866994@qq.com>
2024-02-17 22:15:16 +0800

    bean 配置

--- im-api/src/main/java/com/bx/api/IMApiApplication.java
+++ im-api/src/main/java/com/bx/api/IMApiApplication.java
@@ -1,11 +1,12 @@
 package com.bx.api;
 
 import com.bx.imclient.IMClient;
+import com.bx.imclient.sender.IMSender;
 import io.github.stylesmile.annotation.Fastboot;
 import io.github.stylesmile.app.App;
 // IMClient在子包中，需要添加
 
-@Fastboot(include = {IMClient.class})
+@Fastboot(include = {IMClient.class, IMSender.class})
 public class IMApiApplication {
 
     public static void main(String[] args) {
