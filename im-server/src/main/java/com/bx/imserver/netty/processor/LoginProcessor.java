21e2f01848fa72d2b7a006a92df6584071966863
Stylesmile <3239866994@qq.com>
2024-02-18 00:23:18 +0800

    websocket用户信息初始化

--- im-server/src/main/java/com/bx/imserver/netty/processor/LoginProcessor.java
+++ im-server/src/main/java/com/bx/imserver/netty/processor/LoginProcessor.java
@@ -1,17 +1,15 @@
 package com.bx.imserver.netty.processor;
 
 import cn.hutool.core.bean.BeanUtil;
-import com.alibaba.fastjson.JSON;
 import com.bx.imcommon.contant.IMConstant;
 import com.bx.imcommon.contant.IMRedisKey;
 import com.bx.imcommon.enums.IMCmdType;
 import com.bx.imcommon.model.IMLoginInfo;
 import com.bx.imcommon.model.IMSendInfo;
-import com.bx.imcommon.model.IMSessionInfo;
-import com.bx.imcommon.util.JwtUtil;
 import com.bx.imserver.constant.ChannelAttrKey;
 import com.bx.imserver.netty.IMServerGroup;
 import com.bx.imserver.netty.UserChannelCtxMap;
+import com.google.gson.JsonObject;
 import io.github.stylesmile.annotation.AutoWired;
 import io.github.stylesmile.annotation.Service;
 import io.github.stylesmile.ioc.Value;
@@ -27,7 +25,7 @@ import java.util.HashMap;
 public class LoginProcessor extends AbstractMessageProcessor<IMLoginInfo> {
 
     @AutoWired
-    private  JedisTemplate redisTemplate;
+    private JedisTemplate redisTemplate;
 
     @Value("${jwt.accessToken.secret}")
     private String accessTokenSecret;
@@ -37,18 +35,23 @@ public class LoginProcessor extends AbstractMessageProcessor<IMLoginInfo> {
 
     @Override
     public synchronized void process(ChannelHandlerContext ctx, IMLoginInfo loginInfo) {
-        Object userSession = redisTemplate.getSerializeData(
-                String.format(IMRedisKey.TOKEN_USER_SESSION, loginInfo.getAccessToken()),
-                Object.class);
+        String token = loginInfo.getAccessToken();
+        JsonObject userSession = redisTemplate.getSerializeData(
+                String.format(IMRedisKey.TOKEN_USER_SESSION, token),
+                JsonObject.class);
 //        if (!JwtUtil.checkSign(loginInfo.getAccessToken(), accessTokenSecret)) {
         if (userSession == null) {
             ctx.channel().close();
             log.warn("用户token校验不通过，强制下线,token:{}", loginInfo.getAccessToken());
         }
-        String strInfo = JwtUtil.getInfo(loginInfo.getAccessToken());
-        IMSessionInfo sessionInfo = JSON.parseObject(strInfo, IMSessionInfo.class);
-        Long userId = sessionInfo.getUserId();
-        Integer terminal = sessionInfo.getTerminal();
+//        String strInfo = JwtUtil.getInfo(loginInfo.getAccessToken());
+//        String strInfo = JwtUtil.getInfo(loginInfo.getAccessToken());
+//        IMSessionInfo sessionInfo = JSON.parseObject(strInfo, IMSessionInfo.class);
+//        IMSessionInfo sessionInfo = new IMSessionInfo();
+//        Long userId = sessionInfo.getUserId();
+//        Integer terminal = sessionInfo.getTerminal();
+        Long userId = userSession.get("userId").getAsLong();
+        Integer terminal = userSession.get("terminal").getAsInt();
         log.info("用户登录，userId:{}", userId);
         ChannelHandlerContext context = UserChannelCtxMap.getChannelCtx(userId, terminal);
         if (context != null && !ctx.channel().id().equals(context.channel().id())) {
@@ -73,7 +76,7 @@ public class LoginProcessor extends AbstractMessageProcessor<IMLoginInfo> {
         // 在redis上记录每个user的channelId，15秒没有心跳，则自动过期
         String key = String.join(":", IMRedisKey.IM_USER_SERVER_ID, userId.toString(), terminal.toString());
 //        redisTemplate.opsForValue().set(key, IMServerGroup.serverId, IMConstant.ONLINE_TIMEOUT_SECOND, TimeUnit.SECONDS);
-        redisTemplate.setSerializeDataEx(key, IMServerGroup.serverId, (int)IMConstant.ONLINE_TIMEOUT_SECOND);
+        redisTemplate.setSerializeDataEx(key, IMServerGroup.serverId, (int) IMConstant.ONLINE_TIMEOUT_SECOND);
         // 响应ws
         IMSendInfo<Object> sendInfo = new IMSendInfo<>();
         sendInfo.setCmd(IMCmdType.LOGIN.code());
