70b2b7c357a0c34e6b63182c2bebb1655a5393a7
Stylesmile <3239866994@qq.com>
2024-02-20 21:06:51 +0800

    LoginService

--- im-server/src/main/java/com/bx/imserver/websocket/service/LoginService.java
+++ im-server/src/main/java/com/bx/imserver/websocket/service/LoginService.java
@@ -1,19 +1,23 @@
 package com.bx.imserver.websocket.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.bx.imcommon.contant.IMConstant;
 import com.bx.imcommon.contant.IMRedisKey;
+import com.bx.imcommon.enums.IMCmdType;
 import com.bx.imcommon.model.IMLoginInfo;
-import com.bx.imserver.netty.UserChannelCtxMap;
+import com.bx.imcommon.model.IMSendInfo;
+import com.bx.imserver.netty.IMServerGroup;
 import com.google.gson.JsonObject;
 import io.github.stylesmile.annotation.AutoWired;
 import io.github.stylesmile.annotation.Service;
-import io.github.stylesmile.ioc.Value;
 import io.github.stylesmile.jedis.JedisTemplate;
-import io.netty.channel.ChannelHandlerContext;
+import io.github.stylesmile.tool.JsonGsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.tio.core.ChannelContext;
 import org.tio.core.Tio;
+import org.tio.websocket.common.WsResponse;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 @Slf4j
@@ -41,8 +45,8 @@ public class LoginService {
         Long userId = userSession.get("userId").getAsLong();
         Integer terminal = userSession.get("terminal").getAsInt();
         log.info("用户登录，userId:{}", userId);
-        ChannelHandlerContext context = UserChannelCtxMap.getChannelCtx(userId, terminal);
-//        if (context != null && !ctx.channel().id().equals(context.channel().id())) {
+//        ChannelHandlerContext context = UserChannelCtxMap.getChannelCtx(userId, terminal);
+//        if (channelContext != null && !ctx.channel().id().equals(context.channel().id())) {
 //            // 不允许多地登录,强制下线
 //            IMSendInfo<Object> sendInfo = new IMSendInfo<>();
 //            sendInfo.setCmd(IMCmdType.FORCE_LOGUT.code());
@@ -50,7 +54,9 @@ public class LoginService {
 //            context.channel().writeAndFlush(sendInfo);
 //            log.info("异地登录，强制下线,userId:{}", userId);
 //        }
+
 //        // 绑定用户和channel
+        Tio.bindUser(channelContext, userId.toString());
 //        UserChannelCtxMap.addChannelCtx(userId, terminal, ctx);
 //        // 设置用户id属性
 //        AttributeKey<Long> userIdAttr = AttributeKey.valueOf(ChannelAttrKey.USER_ID);
@@ -62,13 +68,14 @@ public class LoginService {
 //        AttributeKey<Long> heartBeatAttr = AttributeKey.valueOf("HEARTBEAt_TIMES");
 //        ctx.channel().attr(heartBeatAttr).set(0L);
 //        // 在redis上记录每个user的channelId，15秒没有心跳，则自动过期
-//        String key = String.join(":", IMRedisKey.IM_USER_SERVER_ID, userId.toString(), terminal.toString());
-////        redisTemplate.opsForValue().set(key, IMServerGroup.serverId, IMConstant.ONLINE_TIMEOUT_SECOND, TimeUnit.SECONDS);
-//        redisTemplate.setSerializeDataEx(key, IMServerGroup.serverId, (int) IMConstant.ONLINE_TIMEOUT_SECOND);
-//        // 响应ws
-//        IMSendInfo<Object> sendInfo = new IMSendInfo<>();
-//        sendInfo.setCmd(IMCmdType.LOGIN.code());
-//        ctx.channel().writeAndFlush(sendInfo);
+        String key = String.join(":", IMRedisKey.IM_USER_SERVER_ID, userId.toString(), terminal.toString());
+//        redisTemplate.opsForValue().set(key, IMServerGroup.serverId, IMConstant.ONLINE_TIMEOUT_SECOND, TimeUnit.SECONDS);
+        redisTemplate.setSerializeDataEx(key, IMServerGroup.serverId, (int) IMConstant.ONLINE_TIMEOUT_SECOND);
+        // 响应ws
+        IMSendInfo<Object> sendInfo = new IMSendInfo<>();
+        sendInfo.setCmd(IMCmdType.LOGIN.code());
+        WsResponse wsResponse = WsResponse.fromText(JsonGsonUtil.BeanToJson(sendInfo), StandardCharsets.UTF_8.toString());
+        Tio.sendToUser(channelContext.tioConfig, userId.toString(), wsResponse);
     }
 
 
