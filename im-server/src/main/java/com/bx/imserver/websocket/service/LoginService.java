13d704881138dc7d3f89710b27b807d2dbe3ddbb
Stylesmile <3239866994@qq.com>
2024-02-20 20:11:34 +0800

    LoginService.java

--- im-server/src/main/java/com/bx/imserver/websocket/service/LoginService.java
+++ im-server/src/main/java/com/bx/imserver/websocket/service/LoginService.java
@@ -1,13 +1,8 @@
 package com.bx.imserver.websocket.service;
 
 import cn.hutool.core.bean.BeanUtil;
-import com.bx.imcommon.contant.IMConstant;
 import com.bx.imcommon.contant.IMRedisKey;
-import com.bx.imcommon.enums.IMCmdType;
 import com.bx.imcommon.model.IMLoginInfo;
-import com.bx.imcommon.model.IMSendInfo;
-import com.bx.imserver.constant.ChannelAttrKey;
-import com.bx.imserver.netty.IMServerGroup;
 import com.bx.imserver.netty.UserChannelCtxMap;
 import com.google.gson.JsonObject;
 import io.github.stylesmile.annotation.AutoWired;
@@ -15,8 +10,9 @@ import io.github.stylesmile.annotation.Service;
 import io.github.stylesmile.ioc.Value;
 import io.github.stylesmile.jedis.JedisTemplate;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.util.AttributeKey;
 import lombok.extern.slf4j.Slf4j;
+import org.tio.core.ChannelContext;
+import org.tio.core.Tio;
 
 import java.util.HashMap;
 
@@ -33,15 +29,14 @@ public class LoginService {
 
     private String accessTokenSecret2;
 
-    public synchronized void process(ChannelHandlerContext ctx, IMLoginInfo loginInfo) {
-        String token = loginInfo.getAccessToken();
+    public synchronized void process(String token, ChannelContext channelContext) {
         JsonObject userSession = redisTemplate.getSerializeData(
                 String.format(IMRedisKey.TOKEN_USER_SESSION, token),
                 JsonObject.class);
 //        if (!JwtUtil.checkSign(loginInfo.getAccessToken(), accessTokenSecret)) {
         if (userSession == null) {
-            ctx.channel().close();
-            log.warn("用户token校验不通过，强制下线,token:{}", loginInfo.getAccessToken());
+            Tio.remove(channelContext, "receive close flag");
+            log.debug("用户token校验不通过，强制下线,token:{}", token);
         }
 //        String strInfo = JwtUtil.getInfo(loginInfo.getAccessToken());
 //        String strInfo = JwtUtil.getInfo(loginInfo.getAccessToken());
@@ -53,33 +48,33 @@ public class LoginService {
         Integer terminal = userSession.get("terminal").getAsInt();
         log.info("用户登录，userId:{}", userId);
         ChannelHandlerContext context = UserChannelCtxMap.getChannelCtx(userId, terminal);
-        if (context != null && !ctx.channel().id().equals(context.channel().id())) {
-            // 不允许多地登录,强制下线
-            IMSendInfo<Object> sendInfo = new IMSendInfo<>();
-            sendInfo.setCmd(IMCmdType.FORCE_LOGUT.code());
-            sendInfo.setData("您已在其他地方登陆，将被强制下线");
-            context.channel().writeAndFlush(sendInfo);
-            log.info("异地登录，强制下线,userId:{}", userId);
-        }
-        // 绑定用户和channel
-        UserChannelCtxMap.addChannelCtx(userId, terminal, ctx);
-        // 设置用户id属性
-        AttributeKey<Long> userIdAttr = AttributeKey.valueOf(ChannelAttrKey.USER_ID);
-        ctx.channel().attr(userIdAttr).set(userId);
-        // 设置用户终端类型
-        AttributeKey<Integer> terminalAttr = AttributeKey.valueOf(ChannelAttrKey.TERMINAL_TYPE);
-        ctx.channel().attr(terminalAttr).set(terminal);
-        // 初始化心跳次数
-        AttributeKey<Long> heartBeatAttr = AttributeKey.valueOf("HEARTBEAt_TIMES");
-        ctx.channel().attr(heartBeatAttr).set(0L);
-        // 在redis上记录每个user的channelId，15秒没有心跳，则自动过期
-        String key = String.join(":", IMRedisKey.IM_USER_SERVER_ID, userId.toString(), terminal.toString());
-//        redisTemplate.opsForValue().set(key, IMServerGroup.serverId, IMConstant.ONLINE_TIMEOUT_SECOND, TimeUnit.SECONDS);
-        redisTemplate.setSerializeDataEx(key, IMServerGroup.serverId, (int) IMConstant.ONLINE_TIMEOUT_SECOND);
-        // 响应ws
-        IMSendInfo<Object> sendInfo = new IMSendInfo<>();
-        sendInfo.setCmd(IMCmdType.LOGIN.code());
-        ctx.channel().writeAndFlush(sendInfo);
+//        if (context != null && !ctx.channel().id().equals(context.channel().id())) {
+//            // 不允许多地登录,强制下线
+//            IMSendInfo<Object> sendInfo = new IMSendInfo<>();
+//            sendInfo.setCmd(IMCmdType.FORCE_LOGUT.code());
+//            sendInfo.setData("您已在其他地方登陆，将被强制下线");
+//            context.channel().writeAndFlush(sendInfo);
+//            log.info("异地登录，强制下线,userId:{}", userId);
+//        }
+//        // 绑定用户和channel
+//        UserChannelCtxMap.addChannelCtx(userId, terminal, ctx);
+//        // 设置用户id属性
+//        AttributeKey<Long> userIdAttr = AttributeKey.valueOf(ChannelAttrKey.USER_ID);
+//        ctx.channel().attr(userIdAttr).set(userId);
+//        // 设置用户终端类型
+//        AttributeKey<Integer> terminalAttr = AttributeKey.valueOf(ChannelAttrKey.TERMINAL_TYPE);
+//        ctx.channel().attr(terminalAttr).set(terminal);
+//        // 初始化心跳次数
+//        AttributeKey<Long> heartBeatAttr = AttributeKey.valueOf("HEARTBEAt_TIMES");
+//        ctx.channel().attr(heartBeatAttr).set(0L);
+//        // 在redis上记录每个user的channelId，15秒没有心跳，则自动过期
+//        String key = String.join(":", IMRedisKey.IM_USER_SERVER_ID, userId.toString(), terminal.toString());
+////        redisTemplate.opsForValue().set(key, IMServerGroup.serverId, IMConstant.ONLINE_TIMEOUT_SECOND, TimeUnit.SECONDS);
+//        redisTemplate.setSerializeDataEx(key, IMServerGroup.serverId, (int) IMConstant.ONLINE_TIMEOUT_SECOND);
+//        // 响应ws
+//        IMSendInfo<Object> sendInfo = new IMSendInfo<>();
+//        sendInfo.setCmd(IMCmdType.LOGIN.code());
+//        ctx.channel().writeAndFlush(sendInfo);
     }
 
 
