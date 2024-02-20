743aed709cb5ec1653e68190b04d773444b7389d
Stylesmile <3239866994@qq.com>
2024-02-20 21:50:14 +0800

    WsMsgHandler

--- im-server/src/main/java/com/bx/imserver/websocket/WsMsgHandler.java
+++ im-server/src/main/java/com/bx/imserver/websocket/WsMsgHandler.java
@@ -1,5 +1,8 @@
 package com.bx.imserver.websocket;
 
+import com.bx.imcommon.contant.IMRedisKey;
+import com.bx.imserver.websocket.service.DealWebsocketMessageService;
+import com.bx.imserver.websocket.service.HeartbeatService;
 import com.bx.imserver.websocket.service.LoginService;
 import com.google.gson.JsonObject;
 import io.github.stylesmile.tool.FastbootUtil;
@@ -16,6 +19,9 @@ import org.tio.websocket.common.WsSessionContext;
 import org.tio.websocket.server.handler.IWsMsgHandler;
 
 import java.util.Objects;
+
+import static com.bx.imcommon.enums.IMCmdType.HEART_BEAT;
+import static com.bx.imcommon.enums.IMCmdType.LOGIN;
 
 /**
  * @author tanyaowu
@@ -87,44 +93,30 @@ public class WsMsgHandler implements IWsMsgHandler {
      */
     @Override
     public Object onText(WsRequest wsRequest, String text, ChannelContext channelContext) throws Exception {
-        JsonObject jsonObject = JsonGsonUtil.GsonToBean(text, JsonObject.class);
-        Integer cmd = jsonObject.get("cmd").getAsInt();
-        if (null == cmd) {
-            return null;
-        }
-        String token = jsonObject.get("data").getAsJsonObject().get("accessToken").getAsString();
+        DealWebsocketMessageService dealWebsocketMessageService = FastbootUtil.getBean(DealWebsocketMessageService.class);
+        dealWebsocketMessageService.deal(wsRequest,text,channelContext);
 
-        switch (cmd) {
-            case 0:
-                LoginService loginService = FastbootUtil.getBean(LoginService.class);
-                loginService.process(token, channelContext);
-                break;
-            case 1:
-                FastbootUtil.getBean(LoginService.class).process(jsonObject.get("accessToken").getAsString(), channelContext);
-            default:
-                break;
-        }
 
-        WsSessionContext wsSessionContext = (WsSessionContext) channelContext.get();
-        HttpRequest httpRequest = wsSessionContext.getHandshakeRequest();//获取websocket握手包
-        if (log.isDebugEnabled()) {
-            log.debug("握手包:{}", httpRequest);
-        }
-
-//		log.info("收到ws消息:{}", text);
-
-        if (Objects.equals("心跳内容", text)) {
-            return null;
-        }
-        //channelContext.getToken()
-        //String msg = channelContext.getClientNode().toString() + " 说：" + text;
-        String msg = "{name:'" + channelContext.userid + "',message:'" + text + "'}";
-        //用tio-websocket，服务器发送到客户端的Packet都是WsResponse
-        WsResponse wsResponse = WsResponse.fromText(msg, WsConstant.CHARSET);
-        //群发
-        Tio.sendToGroup(channelContext.tioConfig, WsConstant.GROUP_ID, wsResponse);
-
-        //返回值是要发送给客户端的内容，一般都是返回null
+//        WsSessionContext wsSessionContext = (WsSessionContext) channelContext.get();
+//        HttpRequest httpRequest = wsSessionContext.getHandshakeRequest();//获取websocket握手包
+//        if (log.isDebugEnabled()) {
+//            log.debug("握手包:{}", httpRequest);
+//        }
+//
+////		log.info("收到ws消息:{}", text);
+//
+//        if (Objects.equals("心跳内容", text)) {
+//            return null;
+//        }
+//        //channelContext.getToken()
+//        //String msg = channelContext.getClientNode().toString() + " 说：" + text;
+//        String msg = "{name:'" + channelContext.userid + "',message:'" + text + "'}";
+//        //用tio-websocket，服务器发送到客户端的Packet都是WsResponse
+//        WsResponse wsResponse = WsResponse.fromText(msg, WsConstant.CHARSET);
+//        //群发
+//        Tio.sendToGroup(channelContext.tioConfig, WsConstant.GROUP_ID, wsResponse);
+//
+//        //返回值是要发送给客户端的内容，一般都是返回null
         return null;
     }
 
