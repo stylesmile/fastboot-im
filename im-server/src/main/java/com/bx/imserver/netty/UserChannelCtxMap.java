5c251c20cee9fc8cbf0de412a49fd97ef3d3db8b
Stylesmiel <3239866994@qq.com>
2024-02-21 11:53:18 +0800

    websocket上下文映射关系处理

--- im-server/src/main/java/com/bx/imserver/netty/UserChannelCtxMap.java
+++ im-server/src/main/java/com/bx/imserver/netty/UserChannelCtxMap.java
@@ -11,35 +11,21 @@ public class UserChannelCtxMap {
     /**
      *  维护userId和ctx的关联关系，格式:Map<userId,map<terminal，ctx>>
      */
-    private static Map<Long, Map<Integer, ChannelHandlerContext>> channelMap = new ConcurrentHashMap();
-    private static Map<Long, ConcurrentHashMap<Integer, ChannelContext>> channelMap2 = new ConcurrentHashMap();
-
-    public static void addChannelCtx(Long userId, Integer channel, ChannelHandlerContext ctx) {
-        channelMap.computeIfAbsent(userId, key -> new ConcurrentHashMap()).put(channel, ctx);
-    }
+    private static Map<Long, ConcurrentHashMap<Integer, ChannelContext>> channelMap = new ConcurrentHashMap();
     public static void addChannelCtx2(Long userId, Integer channel, ChannelContext ctx) {
-        channelMap2.computeIfAbsent(userId, key -> new ConcurrentHashMap()).put(channel, ctx);
+        channelMap.computeIfAbsent(userId, key -> new ConcurrentHashMap()).put(channel, ctx);
     }
 
     public static void removeChannelCtx(Long userId, Integer terminal) {
         if (userId != null && terminal != null && channelMap.containsKey(userId)) {
-            Map<Integer, ChannelHandlerContext> userChannelMap = channelMap.get(userId);
+            Map<Integer, ChannelContext> userChannelMap = channelMap.get(userId);
             userChannelMap.remove(terminal);
         }
     }
 
-    public static ChannelHandlerContext getChannelCtx(Long userId, Integer terminal) {
-        if (userId != null && terminal != null && channelMap.containsKey(userId)) {
-            Map<Integer, ChannelHandlerContext> userChannelMap = channelMap.get(userId);
-            if (userChannelMap.containsKey(terminal)) {
-                return userChannelMap.get(terminal);
-            }
-        }
-        return null;
-    }
     public static ChannelContext getChannelCtx2(Long userId, Integer terminal) {
-        if (userId != null && terminal != null && channelMap2.containsKey(userId)) {
-            Map<Integer, ChannelContext> userChannelMap = channelMap2.get(userId);
+        if (userId != null && terminal != null && channelMap.containsKey(userId)) {
+            Map<Integer, ChannelContext> userChannelMap = channelMap.get(userId);
             if (userChannelMap.containsKey(terminal)) {
                 return userChannelMap.get(terminal);
             }
@@ -47,11 +33,10 @@ public class UserChannelCtxMap {
         return null;
     }
 
-    public static Map<Integer, ChannelHandlerContext> getChannelCtx(Long userId) {
+    public static Map<Integer, ChannelContext> getChannelCtx(Long userId) {
         if (userId == null) {
             return null;
         }
         return channelMap.get(userId);
     }
-
 }
